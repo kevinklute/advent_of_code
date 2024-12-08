e916eb8802ed669729a67a9c95ae66efda659447
Kevin Klute <kevin@kevinklute.com>
2024-12-08 11:16:43 -0500

    Day3 part2 tidying - remove empty regex matches and add list comprehension

--- 2024/day3/part2_solution.py
+++ 2024/day3/part2_solution.py
@@ -2,11 +2,6 @@ import sys
 import re
 
 filename = sys.argv[1]
-total = 0
-
 with open(filename) as file:
-    muls = re.findall(r'(?:don\'t\(\).*?(?:\Z|do\(\)))|(?:mul\((\d{1,3}),(\d{1,3})\))', file.read(), re.DOTALL)
-for mul in muls:
-    if mul[0] != '':
-        total += int(mul[0]) * int(mul[1])
-print(total)
+    muls = re.findall(r'(?:don\'t\(\).*?(?:\Z|do\(\)).*?)*mul\((\d{1,3}),(\d{1,3})\)', file.read(), re.DOTALL)
+print(sum(map(lambda m: int(m[0]) * int(m[1]), muls)))
